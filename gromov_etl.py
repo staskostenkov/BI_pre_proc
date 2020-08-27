f1bf82c6faa5efb0807d2ccd262378c9fd3db3bb
Stas <38358959+staskostenkov@users.noreply.github.com>
2020-08-27 10:38:35 +0300

    Update gromov_etl.py

--- gromov_etl.py
+++ gromov_etl.py
@@ -1,14 +1,145 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+import pandas as pd
+import numpy as np
+
+# data loading
+countries = data = pd.read_csv('country_nationality.csv')
+data = pd.read_csv('2people_wiki.csv')
+
+# data filtering for columns
+countries = countries[['Country', 'Nationality']]
+countries['Nationality'] = countries['Nationality'].str.lower()
+data = data[['Name', 'Article',]]
+
+# control null-values
+print('countries null', countries.isnull().any)
+print('data null', data.isnull().any)
+
+# data viewing
+#data = data.drop([0])
+prit(data[:3])
+print(countries[:3])
+
+# manth list
+months = ['january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'augest',
+        'september',
+        'november',
+        'december']
+
+# month dictionary for maping
+months_dict = {
+    'january'  : 'Jan',
+    'february' : 'Feb',
+    'march'    : 'Mar',
+    'april'    : 'Apr',
+    'may'      : 'May',
+    'june'     : 'Jun',
+    'july'     : 'Jul',
+    'augest'   : 'Aug',
+    'september': 'Sep',
+    'october'  : 'Oct',
+    'november' : 'Nov',
+    'december' : 'Dec'
+}
+
+# creating country dictionary for maping
+nationality_dict = dict((key, value) for (key, value) in zip(list(countries['Nationality']), 
+                                                             list(countries['Country'])))
+# articles split to words
+words_df = data['Article'].str.split()
+# size control 
+print(words_df.shape[0])
+
+# new columns initialisation
+data['day'] = 0
+data['month'] = ''
+data['year'] = 0
+data['status'] = ''
+data['Gender'] = ''
+data['Nationality'] = ''
+data['Country'] = ''
 
 
-get_ipython().run_cell_magic('time', '', "import pandas as pd\nimport numpy as np\n\n# data loading\ncountries = data = pd.read_csv('country_nationality.csv', nrows=5*10**6)\ndata = pd.read_csv('2people_wiki.csv', nrows=5*10**6)\n\n# data filtering for columns\ncountries = countries[['Country', 'Nationality']]\ncountries['Nationality'] = countries['Nationality'].str.lower()\ndata = data[['Name', 'Article',]]\n\n# control null-values\nprint('countries null', countries.isnull().any)\nprint('data null', data.isnull().any)\n\n# data viewing\n#data = data.drop([0])\nprit(data[:3])\nprint(countries[:3])\n\n# manth list\nmonths = ['january',\n        'february',\n        'march',\n        'april',\n        'may',\n        'june',\n        'july',\n        'augest',\n        'september',\n        'november',\n        'december']\n\n# month dictionary for maping\nmonths_dict = {\n    'january'  : 'Jan',\n    'february' : 'Feb',\n    'march'    : 'Mar',\n    'april'    : 'Apr',\n    'may'      : 'May',\n    'june'     : 'Jun',\n    'july'     : 'Jul',\n    'augest'   : 'Aug',\n    'september': 'Sep',\n    'october'  : 'Oct',\n    'november' : 'Nov',\n    'december' : 'Dec'\n}\n\n# creating country dictionary for maping\nnationality_dict = dict((key, value) for (key, value) in zip(list(countries['Nationality']), \n                                                             list(countries['Country'])))\n# articles split to words\nwords_df = data['Article'].str.split()\n# size control \nprint(words_df.shape[0])\n\n# new columns initialisation\ndata['day'] = 0\ndata['month'] = ''\ndata['year'] = 0\ndata['status'] = ''\ndata['Gender'] = ''\ndata['Nationality'] = ''\ndata['Country'] = ''\n\n\nfor ii in range(words_df.shape[0]):\n    try:\n        if 'born' in words_df.iloc[ii]:\n            iin = words_df.iloc[ii].index('born')\n            if words_df.iloc[ii][iin+1] == 'on':\n                day = words_df.iloc[ii][iin+3]\n                month = words_df.iloc[ii][iin+2]\n                year = words_df.iloc[ii][iin+4]\n            else:\n                day = words_df.iloc[ii][iin+1]\n                month = words_df.iloc[ii][iin+2]\n                year = words_df.iloc[ii][iin+3]\n        if 'she' in words_df.iloc[ii]:\n            gender = 'Female'\n        if 'he' in words_df.iloc[ii]:\n            gender = 'Male'\n\n            if (month not in months) & (day in months):\n                month, day = day, month\n\n            try:\n                year = int(year)\n            except ValueError:\n                year = 3999\n\n            try:\n                day = int(day)\n            except ValueError:\n                day = 77\n\n            if day >100:\n                    year = day\n                    day = 0\n\n            # vars recording to dataframe\n            if month in months:\n                data['day'].iloc[ii] = day\n                data['month'].iloc[ii] = month\n                data['year'].iloc[ii] = year\n                data['Gender'].iloc[ii] = gender\n                data['status'].iloc[ii] = 'good'\n\n            # nationality finding, distance checking\n            try:\n                min_index_number = 10000\n                born_index_number = words_df.iloc[ii].index('born')\n                for nationality in list(countries.Nationality):\n                    if nationality in data['Article'].iloc[ii]:\n                        if (words_df.iloc[ii].index(nationality)-born_index_number)<min_index_number:\n                            min_index_number = words_df.iloc[ii].index(nationality)-born_index_number\n                            my_nationality = nationality\n                data['Nationality'].iloc[ii] = my_nationality\n                #data['Country'].iloc[ii] = data['Nationality'].iloc[ii].map(nationality_dict)\n            except ValueError:\n                pass\n            \n            print(ii, True, iin, month, day, year)\n    except IndexError:\n        pass\n    \ndata['Date'] = ''\n# month maping to a short format\ndata['Date'] = data['month'].map(months_dict)\n# data format creating\ndata['Date'] = data['day'].astype(str) +'-'+data['Date']+'-'+data['year'].astype(str)\n\n# not-null data filtering\ndata = data[data.year>0]\n# nationality maping to country\ndata['Country'] = data1['Nationality'].map(nationality_dict)\n\n# data saveing to a file\ndata[['Name','day','month','year','Gender', 'Country', 'Date']].to_csv('out_data.csv')\ndata[:6]")
+for ii in range(words_df.shape[0]):
+    try:
+        if 'born' in words_df.iloc[ii]:
+            iin = words_df.iloc[ii].index('born')
+            if words_df.iloc[ii][iin+1] == 'on':
+                day = words_df.iloc[ii][iin+3]
+                month = words_df.iloc[ii][iin+2]
+                year = words_df.iloc[ii][iin+4]
+            else:
+                day = words_df.iloc[ii][iin+1]
+                month = words_df.iloc[ii][iin+2]
+                year = words_df.iloc[ii][iin+3]
+        if 'she' in words_df.iloc[ii]:
+            gender = 'Female'
+        if 'he' in words_df.iloc[ii]:
+            gender = 'Male'
 
+            if (month not in months) & (day in months):
+                month, day = day, month
 
-# In[ ]:
+            try:
+                year = int(year)
+            except ValueError:
+                year = 3999
 
+            try:
+                day = int(day)
+            except ValueError:
+                day = 77
 
+            if day >100:
+                    year = day
+                    day = 0
 
+            # vars recording to dataframe
+            if month in months:
+                data['day'].iloc[ii] = day
+                data['month'].iloc[ii] = month
+                data['year'].iloc[ii] = year
+                data['Gender'].iloc[ii] = gender
+                data['status'].iloc[ii] = 'good'
 
+            # nationality finding, distance checking
+            try:
+                min_index_number = 10000
+                born_index_number = words_df.iloc[ii].index('born')
+                for nationality in list(countries.Nationality):
+                    if nationality in data['Article'].iloc[ii]:
+                        if (words_df.iloc[ii].index(nationality)-born_index_number)<min_index_number:
+                            min_index_number = words_df.iloc[ii].index(nationality)-born_index_number
+                            my_nationality = nationality
+                data['Nationality'].iloc[ii] = my_nationality
+                #data['Country'].iloc[ii] = data['Nationality'].iloc[ii].map(nationality_dict)
+            except ValueError:
+                pass
+            
+            print(ii, True, iin, month, day, year)
+    except IndexError:
+        pass
+    
+data['Date'] = ''
+# month maping to a short format
+data['Date'] = data['month'].map(months_dict)
+# data format creating
+data['Date'] = data['day'].astype(str) +'-'+data['Date']+'-'+data['year'].astype(str)
+
+# not-null data filtering
+data = data[data.year>0]
+# nationality maping to country
+data['Country'] = data1['Nationality'].map(nationality_dict)
+
+# data saveing to a file
+data[['Name','day','month','year','Gender', 'Country', 'Date']].to_csv('out_data.csv')
+print(data[:6])
